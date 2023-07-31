809a4dd5c525369d61c9ea75f9205a59265b14fd
Sumed-Sharma <122160068+Sumed-Sharma@users.noreply.github.com>
2023-07-31 17:24:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADk44vlj33bHxKCWJWDQvnJ2Y-_y9WXd4",
+  authDomain: "pro-60-e937b.firebaseapp.com",
+  projectId: "pro-60-e937b",
+  storageBucket: "pro-60-e937b.appspot.com",
+  messagingSenderId: "68009921793",
+  appId: "1:68009921793:web:2618d194e4c8d5f2135f00"
+};
+ export default firebase.database()
+ 
+
+  
+
